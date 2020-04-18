6a5f20b0c5f4849e80dbec4b90f2c8b389f9d3ff
Todd Urbanowicz <urbo425@gmail.com>
2020-04-18 02:08:42 -0400

    chore(styling): add matblazor components to app

---
+++ PoePricing/Stashes/GetStashTabItems.cs
@@ -0,0 +1,27 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+using MediatR;
+
+namespace PoePricing.Stashes
+{
+    public static class GetStashTabItems
+    {
+        public class Request : IRequest<Response>
+        {
+            public string TabIndex { get; set; }
+        }
+
+        public class Response
+        {
+
+        }
+
+        public class Handler : IRequestHandler<Request, Response>
+        {
+            public Task<Response> Handle(Request request, CancellationToken cancellationToken)
+            {
+                throw new System.NotImplementedException();
+            }
+        }
+    }
+}
